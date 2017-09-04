3d0bf165ff48e428f36c70a8e6cdea8de7dc4a68
CARMONA FERRI Javier <javcarfer92@gmail.com>
2017-09-04 13:38:04 +0200

    Añadidos iconos y menú principal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router,Route, Link, Switch} from 'react-router-dom';
-import {Navbar, NavItem} from 'react-materialize';
+import {Navbar, NavItem, SideNav, SideNavItem, Button, Icon} from 'react-materialize';
 import Home from './initial/Home.js';
 import Login from './initial/Login.js';
 import Signup from './initial/Signup.js';
@@ -9,14 +9,34 @@ const Header =() => {
   return (
     <Navbar right>
       <NavItem>
-        <Link to='/'>Home</Link>
+        <Link to='/'><Icon>home</Icon></Link>
       </NavItem>
       <NavItem>
-        <Link to='/signup'>Sign up</Link>
+        <Link to='/signup'><Icon>group_add</Icon></Link>
       </NavItem>
       <NavItem>  
-        <Link to='/login'>Login</Link>
+        <Link to='/login'><Icon>group</Icon></Link>
       </NavItem>
+      
+        <SideNav
+            trigger={<NavItem><Link to='#'><Icon>menu</Icon></Link></NavItem>}
+            options={{ closeOnClick: true }}
+            >
+            <SideNavItem userView
+              user={{
+                background: 'img/office.jpg',
+                image: 'img/yuna.jpg',
+                name: 'John Doe',
+                email: 'jdandturk@gmail.com'
+              }}
+            />
+            <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+            <SideNavItem href='#!second'>Second Link</SideNavItem>
+            <SideNavItem divider />
+            <SideNavItem subheader>Subheader</SideNavItem>
+            <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+          </SideNav>
+      
     </Navbar>
   );
 
@@ -31,7 +51,6 @@ const App =() => {
         <Route exact path="/" component={Home}/>
         <Route path="/signup" component={Signup}/>
         <Route path="/login" component={Login}/>
-        <h2>Prueba</h2>
       </div>
     </Router>
   );
